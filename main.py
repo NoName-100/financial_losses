5d65e3695bf5ff3a8a60e0b1b2bed00f5eafe79b
Medric Sonwa <medric49@gmail.com>
2021-11-06 16:23:25 -0400

    Update new submission

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import numpy as np
 import pandas as pd
 import pickle
+import random
 
-power = 4
+random.seed(41)
+np.random.seed(41)
+
+power = 1.51
 z_dim = 4
 
 
@@ -17,9 +21,9 @@ def train(data_file):
     return mu, B, power
 
 
-def generator(theta, Z):
+def G(theta, Z):
     mu, B, power = theta
-    generated_data = (mu + np.dot(Z, B.T)) ** power
+    generated_data = np.abs(mu + np.dot(Z, B.T)) ** power
     return generated_data
 
 
@@ -27,24 +31,25 @@ if __name__ == '__main__':
 
     # THETA LOADING
     try:
-        with open('submission/theta.pth', 'rb') as theta_file:
+        with open('theta.pth', 'rb') as theta_file:
             theta = pickle.load(theta_file)
     except FileNotFoundError:
         # Theta training
         theta = train('data/train.csv')
 
-        file = open('submission/theta.pth', 'wb')
+        file = open('theta.pth', 'wb')
         pickle.dump(theta, file)
         file.close()
 
     # NOISE VECTOR LOADING
     try:
-        Z = pd.read_csv('submission/noise.csv', header=None)
+        Z = pd.read_csv('noise.csv', header=None)
     except FileNotFoundError:
         mu_Z = np.zeros(z_dim)
         cov_Z = np.eye(z_dim)
         Z = np.random.multivariate_normal(mu_Z, cov_Z, 410)
+        pd.DataFrame(Z).to_csv('noise.csv', index=False, header=False)
 
     # DATA GENERATION
-    generated = generator(theta, Z)
-    pd.DataFrame(generated).to_csv('generated_data.csv', index=False, header=False)
+    generated = G(theta, Z)
+    pd.DataFrame(generated).to_csv('generated_samples.csv', index=False, header=False)
